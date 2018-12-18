3205e0a26ce32385ac5b44f6d424cf02b1f32223
Yunan Helmy <helmy@extrainteger.com>
2018-12-18 16:18:46 +0700

    refresh & revoke token

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   use_doorkeeper
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount API::InitOauth, at: "/"
   mount API::Init, at: "/auth"
+
   mount GrapeSwaggerRails::Engine, as: "doc", at: "/doc"
 end
