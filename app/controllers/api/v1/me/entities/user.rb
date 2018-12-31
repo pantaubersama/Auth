111791f0183bdfecde86b475291acf92535e18ad
Yunan Helmy <helmy@extrainteger.com>
2018-12-31 14:37:48 +0700

    informant detail

--- app/controllers/api/v1/me/entities/user.rb
+++ app/controllers/api/v1/me/entities/user.rb
@@ -5,4 +5,5 @@ class Api::V1::Me::Entities::User < Api::V1::ValidToken::Entities::User
   expose :education
   expose :occupation
   expose :avatar
+  expose :informant, using: API::V1::Informants::Entities::Informant
 end
