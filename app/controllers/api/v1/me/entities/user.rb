de06bb29929f955dbedac8fa54246f077514d676
Yunan Helmy <helmy@extrainteger.com>
2018-12-31 14:27:16 +0700

    add me detail

--- app/controllers/api/v1/me/entities/user.rb
+++ app/controllers/api/v1/me/entities/user.rb
@@ -1,3 +1,8 @@
 class Api::V1::Me::Entities::User < Api::V1::ValidToken::Entities::User
-  
+  expose :username
+  expose :about
+  expose :location
+  expose :education
+  expose :occupation
+  expose :avatar
 end
