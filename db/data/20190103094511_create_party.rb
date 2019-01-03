6d00a3dfba2dc3e53551ec7a377cb89e2ec15849
Yunan Helmy <helmy@extrainteger.com>
2019-01-03 17:25:20 +0700

    remove

--- db/data/20190103094511_create_party.rb
+++ db/data/20190103094511_create_party.rb
@@ -1,89 +1,93 @@
 class CreateParty < SeedMigration::Migration
   def up
-    data = [
-      {
-        name: "PARTAI KEBANGKITAN BANGSA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/1.jpeg')))
-      },
-      {
-        name: "PARTAI GERAKAN INDONESIA RAYA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/2.jpg')))
-      },
-      {
-        name: "PARTAI DEMOKRASI INDONESIA PERJUANGAN",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/3.jpg')))
-      },
-      {
-        name: "PARTAI GOLONGAN KARYA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/4.jpg')))
-      },
-      {
-        name: "PARTAI NASDEM",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/5.jpg')))
-      },
-      {
-        name: "PARTAI GERAKAN PERUBAHAN INDONESIA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/6.jpg')))
-      },
-      {
-        name: "PARTAI BERKARYA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/7.jpg')))
-      },
-      {
-        name: "PARTAI KEADILAN SEJAHTERA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/8.jpg')))
-      },
-      {
-        name: "PERSATUAN INDONESIA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/8.JPG')))
-      },
-      {
-        name: "PARTAI PERSATUAN PEMBANGUNAN",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/10.jpg')))
-      },
-      {
-        name: "PARTAI SOLIDARITAS INDONESIA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/11.jpeg')))
-      },
-      {
-        name: "PARTAI AMANAT NASIONAL",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/12.jpg')))
-      },
-      {
-        name: "PARTAI HATI NURANI RAKYAT",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/13.jpg')))
-      },
-      {
-        name: "PARTAI DEMOKRAT",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/14.jpg')))
-      },
-      {
-        name: "PARTAI ACEH",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/15.jpg')))
-      },
-      {
-        name: "PARTAI SIRA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/16.jpg')))
-      },
-      {
-        name: "PARTAI DAERAH ACEH",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/17.jpg')))
-      },
-      {
-        name: "PARTAI NANGGROE ACEH",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/18.jpg')))
-      },
-      {
-        name: "PARTAI BULAN BINTANG",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/19.jpg')))
-      },
-      {
-        name: "PARTAI KEADILAN DAN PERSATUAN INDONESIA",
-        image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/20.jpg')))
-      },
-    ]
+    # Run Manually
+    # rake seed:rollback MIGRATION=20190103094511 
+    # rake seed:migrate MIGRATION=20190103094511 
 
-    PoliticalParty.create(data)
+    # data = [
+    #   {
+    #     name: "PARTAI KEBANGKITAN BANGSA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/1.jpeg')))
+    #   },
+    #   {
+    #     name: "PARTAI GERAKAN INDONESIA RAYA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/2.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI DEMOKRASI INDONESIA PERJUANGAN",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/3.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI GOLONGAN KARYA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/4.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI NASDEM",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/5.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI GERAKAN PERUBAHAN INDONESIA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/6.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI BERKARYA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/7.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI KEADILAN SEJAHTERA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/8.jpg')))
+    #   },
+    #   {
+    #     name: "PERSATUAN INDONESIA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/8.JPG')))
+    #   },
+    #   {
+    #     name: "PARTAI PERSATUAN PEMBANGUNAN",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/10.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI SOLIDARITAS INDONESIA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/11.jpeg')))
+    #   },
+    #   {
+    #     name: "PARTAI AMANAT NASIONAL",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/12.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI HATI NURANI RAKYAT",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/13.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI DEMOKRAT",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/14.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI ACEH",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/15.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI SIRA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/16.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI DAERAH ACEH",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/17.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI NANGGROE ACEH",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/18.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI BULAN BINTANG",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/19.jpg')))
+    #   },
+    #   {
+    #     name: "PARTAI KEADILAN DAN PERSATUAN INDONESIA",
+    #     image: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/political_party/20.jpg')))
+    #   },
+    # ]
+
+    # PoliticalParty.create(data)
   end
 
   def down
