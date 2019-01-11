815925b2df9cb0eb1bda04b951abb6d597ae4e84
Yunan Helmy <helmy@extrainteger.com>
2019-01-11 10:56:47 +0700

    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email     = auth.info.email
-      user.full_name = [auth.info.first_name, auth.info.last_name]
+      user.full_name = [auth.info.first_name, auth.info.last_name].join(" ")
 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
