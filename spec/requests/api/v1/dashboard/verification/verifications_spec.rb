14a4c3317e8263fde68e4c36b1db047b28830ac8
myusuf <muhammadyusuf931@gmail.com>
2019-01-25 17:40:27 +0700

    add user verification

---
+++ spec/requests/api/v1/dashboard/verification/verifications_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+RSpec.describe "Api::V1::Dashboard::Verifications", type: :request do
+  let!(:application) { FactoryBot.create :application }
+  let!(:user) { FactoryBot.create :user }
+  let!(:token) { FactoryBot.create :access_token, :application => application, :resource_owner_id => user.id }
+  let!(:yusuf) {FactoryBot.create :user}
+
+
+  before do
+    user.make_me_admin!
+    yusuf.build_verification_model
+  end
+
+  describe "not authorized" do
+    it "cannot add user verification" do
+      post "/dashboard/v1/verifications/user",
+           params:  {
+             email: yusuf.email,
+           }
+      expect(response.status).to eq(401)
+    end
+
+    it "cannot access list user verification" do
+      get "/dashboard/v1/users/verifications",
+        params: {
+          page: 1,
+          q: "*",
+          o: "and",
+          m: "word_start",
+        }
+      expect(response.status).to eq(401)
+    end
+
+  end
+
+  describe "authorized" do
+    it "success verification" do
+      post '/dashboard/v1/verifications/user',
+        params:{
+          email: yusuf.email,
+          ktp_number: "091923981923989",
+          ktp_selfie: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/images/html.png'))),
+          ktp_photo: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/images/html.png'))),
+          signature: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/images/html.png'))),
+        },
+        headers: {Authorization: token.token}
+        expect(response.status).to eq(201)
+        expect(json_response['data']['status']).to eq('verified')
+    end
+
+    it "can access list user verification" do
+      get "/dashboard/v1/users/verifications",
+        params: {
+          page: 1,
+          q: "*",
+          o: "and",
+          m: "word_start",
+        },
+        headers: {Authorization: token.token}
+      expect(response.status).to eq(200)
+    end
+
+  end
+
+
+end
