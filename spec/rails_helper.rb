73bddc85f9fff4df09f517221fdb5145dec9fcf5
alamybs <namakukingkong@gmail.com>
2019-01-27 11:58:14 +0700

    Fix rspec when callback user model changed.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,14 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'webmock/rspec'
 require 'bunny-mock'
+require 'elasticsearch/model'
+require 'elasticsearch/persistence'
+
 BunnyMock::use_bunny_queue_pop_api = true
 WebMock.disable_net_connect!(allow_localhost: true)
+unless defined?(ELASTICSEARCH_URL)
+  ELASTICSEARCH_URL = ENV['ELASTICSEARCH_URL'] || "localhost:#{(ENV['TEST_CLUSTER_PORT'] || 9200)}"
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
