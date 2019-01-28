788a3061c22618bcd7f3794d09bcbe1837b95dfb
Yunan Helmy <helmy@extrainteger.com>
2019-01-28 12:59:25 +0700

    fix firebase content update

--- app/models/firebase_key.rb
+++ app/models/firebase_key.rb
@@ -4,9 +4,11 @@ class FirebaseKey < ApplicationRecord
   belongs_to :user
 
   def self.assign! user_id, tipe, content
-    FirebaseKey.where(user_id: user_id, key_type: FirebaseKey.key_types[tipe]).first_or_create! do |f|
+    firebase = FirebaseKey.where(user_id: user_id, key_type: FirebaseKey.key_types[tipe]).first_or_create! do |f|
       f.content = content
     end
+    firebase.update_attribute(:content, content)
+    firebase
   end
   
 end
