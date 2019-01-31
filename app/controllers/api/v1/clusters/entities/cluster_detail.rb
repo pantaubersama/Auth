d9c98b32985c78fe3ca0dc5b2387210238270aff
Ashar Mubasir <asharmubasir@gmail.com>
2019-01-31 17:39:21 +0700

    Add Created at to cluster

--- app/controllers/api/v1/clusters/entities/cluster_detail.rb
+++ app/controllers/api/v1/clusters/entities/cluster_detail.rb
@@ -6,5 +6,6 @@ module API::V1::Clusters::Entities
     expose :is_link_active
     expose :status
     expose :requester, using: API::V1::Clusters::Entities::Requester, if: lambda { |c,o| o[:current_user].present? && o[:current_user].is_admin? }
+    expose :created_at
   end
 end
