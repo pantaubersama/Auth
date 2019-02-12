fcda77b598e75506cf171022f4238e031a23a5e1
Yunan Helmy <m.yunan.helmy@gmail.com>
2019-02-12 16:58:31 +0700

    git diff

--- spec/requests/api/v1/callback/access_token_spec.rb
+++ spec/requests/api/v1/callback/access_token_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Api::V1::Callback", type: :request do
     let!(:user) { FactoryBot.create :user }
     let!(:token)  { FactoryBot.create :access_token, :application => application, :resource_owner_id => user.id }
     let!(:provider_token) { SecureRandom.hex(32) }
-    
+
     before do
       stub_credentials_request provider_token
       stub_credential_verify provider_token
@@ -16,7 +16,7 @@ RSpec.describe "Api::V1::Callback", type: :request do
       get "/v1/callback", params: {provider_token: provider_token}
       expect(json_response[:data][:access_token]).not_to eq(nil)
       expect(json_response[:data][:refresh_token]).not_to eq(nil)
-      
+
       u = User.find_by email: "helmy@extrainteger.com"
       expect(u.username).to eq("yunanhelmy")
       expect(u.email).to eq("helmy@extrainteger.com")
