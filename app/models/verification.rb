a7237599056f97ba84c96f29706e1e9e22be0d19
Yunan Helmy <m.yunan.helmy@gmail.com>
2019-02-13 21:50:22 +0700

    notifikasi for profile

--- app/models/verification.rb
+++ app/models/verification.rb
@@ -7,6 +7,8 @@ class Verification < ApplicationRecord
   mount_uploader :ktp_photo, AvatarUploader
   mount_uploader :signature, AvatarUploader
 
+  after_save :send_notification
+
   def is_verified?
     verified?
   end
@@ -14,9 +16,21 @@ class Verification < ApplicationRecord
   def step
     return 4 if ktp_number? && ktp_selfie? && ktp_photo? && signature?
     return 3 if ktp_number? && ktp_selfie? && ktp_photo?
-    return 2 if ktp_number? && ktp_selfie? 
+    return 2 if ktp_number? && ktp_selfie?
     return 1 if ktp_number?
   end
-  
-  
+
+  def send_notification
+    if status == "rejected"
+      Publishers::BadgeNotification.publish PROFILE_NOTIFICATION, {
+        receiver_id: user.id, notif_type: :profile, event_type: :gagal_verifikasi
+      }
+    elsif status == "verified"
+      Publishers::BadgeNotification.publish PROFILE_NOTIFICATION, {
+        receiver_id: user.id, notif_type: :profile, event_type: :berhasil_verifikasi
+      }
+    end
+  end
+
+
 end
