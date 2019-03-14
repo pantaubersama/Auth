6b953de1b8205b1e3771769bc171f36808e0cadb
Yunan Helmy <yunanhelmy@Yunan-MacBook-Pro.local>
2019-03-14 15:48:42 +0700

    connect with fb & twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identitas, ENV["API_KEY"], ENV["API_SECRET"], client_options: {
     site: ENV["SYMBOLIC_URL"]
   }
+  provider :twitter, ENV["TW_CONSUMER_KEY"], ENV["TW_CONSUMER_SECRET"]
+  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   use_doorkeeper do
     skip_controllers :authorizations, :authorized_applications, :applications
   end
-  
+
   mount API::Init, at: "/"
   mount API::InitDashboard, at: "/dashboard"
 
   mount GrapeSwaggerRails::Engine, as: "doc", at: "/doc"
+
+  get "/auth/twitter/callback", to: "omniauth#twitter"
+  # get "/auth/facebook/callback", to: "omniauth#facebook"
 end
